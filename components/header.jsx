e687de75692540a4b8ab439a449e0bb9fb5c9587
logx <iamvarun158@gmail.com>
2025-05-27 23:34:50 +0530

    Update headline text color for improved readability

--- components/header.jsx
+++ components/header.jsx
@@ -7,7 +7,7 @@ const Header = (props) => {
         <div className='lg:fixed h-screen basis-1.5/4 flex flex-col justify-between pb-48 self-center lg:self-auto'>
             <div className=''>
                 <h1 className='text-5xl subpixel-antialiased tracking-wide'>{props.data.name}</h1>
-                <h2 className='text-primary-500 pt-2 text-base font-medium tracking-wider brightness-75'>{props.data.headline}</h2>
+                <h2 className='text-primary-300 pt-2 text-base font-medium tracking-wider'>{props.data.headline}</h2>
             </div>
             <Navigation></Navigation>
             <Socials data={props.data.socials}></Socials>
